b8beec594d580d318081acb41840320faedb2fe3
Tasty <131797881+Tim-Eiselt@users.noreply.github.com>
2025-03-08 22:52:13 +0100

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import School
-
-class SchoolAdmin(admin.ModelAdmin):
-    list_display = ('name', 'min_czech_score', 'min_math_score', 'contact_email', 'phone_number', 'link')
-
-admin.site.register(School, SchoolAdmin)
-
